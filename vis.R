9a2e1202c23d38cd8dd3ed2840d782c7b592e4b2
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-03-25 15:38:59 +0100

    *shankar* new repo structure

--- vis.R
+++ vis.R
@@ -9,6 +9,7 @@ library(reshape2)
 data <- read.xls("./data/SIF_Sensors.xlsx",sheet=1)
 data <- data[c(1,9,10)]
 data <- data[grep("multispectral",data[,2]),]
+names(data)[2] <- "Spectral_Bands"
 
 # TODO FWHM stands for diameter type width, not radius, so +- FWHM/2
 # plot the initial ones for now
